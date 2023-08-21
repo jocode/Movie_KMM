b18970e4ac517f9d440af48c1bc532c416499811
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-21 14:42:09 -0500

    Add loadMore function in homeScreen for iOS App

--- iosApp/iosApp/home/HomeScreen.swift
+++ iosApp/iosApp/home/HomeScreen.swift
@@ -5,18 +5,28 @@ struct HomeScreen: View {
     
     @StateObject var viewModel = HomeViewModel()
     
-    let gridColumns: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 12), count: 2)
+    let gridColumns: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 16), count: 2)
 
 	var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: gridColumns, spacing: 12) {
+                LazyVGrid(columns: gridColumns, spacing: 16) {
                     
                     ForEach(viewModel.movies, id: \.id) { movie in
                         MovieGridItem(movie: movie)
+                            .task {
+                                if movie == viewModel.movies.last && !viewModel.isLoading && !viewModel.loadFinished {
+                                    await viewModel.loadMovies()
+                                }
+                            }
+                    }
+                    
+                    if viewModel.isLoading {
+                        Section(footer: ProgressView()) {}
                     }
                     
                 }
+                .padding(.horizontal, 12)
             }
             .navigationTitle("Movies")
         }
